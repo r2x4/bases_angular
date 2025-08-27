9e4fb0a1e7066eb8f09ee2690066eae723b88f80
Rodrigo <rrs230780@gmail.com>
2025-08-27 09:03:01 -0500

    guardarNvbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  templateUrl: './navbar.component.html',
 })
-export class Navbar {
+export class NavbarComponent {
 
 }
