76c90ce926cd53e96da1877cf4b4faadb9027f6f
Rodrigo <rrs230780@gmail.com>
2025-08-27 10:47:48 -0500

    vamos

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,9 +1,22 @@
-import { Component, } from "@angular/core";
+import { Component, signal, } from "@angular/core";
 
 
+
+interface Character {
+    id : number;
+    name: string;
+    power: number;
+}
 
 @Component({
     templateUrl: './dragonball-page.component.html',
 })
 
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+    characters = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 10000 },
+        { id: 2, name: 'Vegeta', power: 20000 },
+        { id: 3, name: 'Gogan', power: 30000 },
+        { id: 4, name: 'Trunks', power: 40000 },
+    ]);
+}
